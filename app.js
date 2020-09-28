6a361550a8f6aa58017886d945a083f10744f8e9
kimth007kim <kimth007kim@naver.com>
2020-09-28 18:10:45 +0900

    css 자리 잡는중 헤더 작업해야함

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 
 const port = 3000;
 
+app.use(express.static("images"));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 app.get("/", (req, res) => {
