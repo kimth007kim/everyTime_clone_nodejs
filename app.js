b20083e339bde84915edb6f2b6408bacd83116e5
kimth007kim <kimth007kim@naver.com>
2020-10-05 15:58:41 +0900

    데베 생성 로그인화면 생성 회원가입화면 생성

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require("express");
 var path = require("path");
+var mysql = require("mysql");
+var client = mysql.createConnection({
+  user: "root",
+  password: "1234",
+  database: "every_time",
+});
 const app = express();
 
 const port = 3000;
@@ -9,6 +15,12 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 app.get("/", (req, res) => {
   res.render("index", { title: "Index" });
+});
+app.get("/login", (req, res) => {
+  res.render("login", { title: "login" });
+});
+app.get("/register", (req, res) => {
+  res.render("register");
 });
 
 app.listen(port, () => {
