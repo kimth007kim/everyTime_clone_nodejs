56c057098c28f4f7cb84b23beeccf1512f817d10
kimth007kim <kimth007kim@naver.com>
2020-10-15 18:47:36 +0900

    모든 게시판 출력 완료

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 var path = require("path");
 var mysql = require("mysql");
-const { connect } = require("http2");
+const e = require("express");
 var client = mysql.createConnection({
   user: "root",
   password: "1234",
   database: "every_time",
+  multipleStatements: true,
 });
+
 client.connect();
 
 const app = express();
@@ -29,6 +31,8 @@ app.set("view engine", "pug");
 //     })
 //   });
 
+// 3번째 시도  두번째 쿼리를 콜백 함수 안에다가 집어넣기
+
 app.get("/", (req, res) => {
   const sql1 = "Select * from free order by f_date DESC limit 4";
   const sql2 = "Select * from secret order by s_date DESC limit 4";
@@ -42,59 +46,141 @@ app.get("/", (req, res) => {
     if (err) {
       console.log(err);
     } else {
-      res.render('index',{frees:frees})
-      }
-    })
-    client.query(sql2, (err, secrets, fields) => {
-      if (err) {
-        console.log(err);
-      } else {
-        res.render('index',{secrets:secrets})
+      client.query(sql2, (err, secrets, fields) => {
+        if (err) {
+          console.log(err);
+        } else {
+          client.query(sql3, (err, grads, fields) => {
+            if (err) {
+              console.log(err);
+            } else {
+              client.query(sql4, (err, markets, fields) => {
+                if (err) {
+                  console.log(err);
+                } else {
+                  client.query(sql5, (err, news, fields) => {
+                    if (err) {
+                      console.log(err);
+                    } else {
+                      client.query(sql6, (err, infos, fields) => {
+                        if (err) {
+                          console.log(err);
+                        } else {
+                          client.query(sql7, (err, proms, fields) => {
+                            if (err) {
+                              console.log(err);
+                            } else {
+                              client.query(sql8, (err, teams, fields) => {
+                                if (err) {
+                                  console.log(err);
+                                } else {
+                                  res.render("index", {
+                                    frees: frees,
+                                    secrets: secrets,
+                                    grads: grads,
+                                    markets: markets,
+                                    news: news,
+                                    infos: infos,
+                                    proms: proms,
+                                    teams: teams,
+                                  });
+                                }
+                              });
+                            }
+                          });
+                        }
+                      });
+                    }
+                  });
+                }
+              });
+            }
+          });
         }
-      })
-    client.query(sql3, (err, grads, fields) => {
-      if (err) {
-        console.log(err);
-      } else {
-        res.render('index',{grads:grads})
-        }
-      })
-    client.query(sql4, (err, markets, fields) => {
-      if (err) {
-        console.log(err);
-      } else {
-        res.render('index',{markets:markets})
-        }
-      })
-    client.query(sql5, (err, news, fields) => {
-      if (err) {
-        console.log(err);
-      } else {
-        res.render('index',{news:news})
-        }
-      })
-    client.query(sql6, (err, infos, fields) => {
-      if (err) {
-        console.log(err);
-      } else {
-        res.render('index',{infos:infos})
-        }
-      })
-    client.query(sql7, (err, proms, fields) => {
-      if (err) {
-        console.log(err);
-      } else {
-        res.render('index',{proms:proms})
-        }
-      })
-    client.query(sql8, (err, teams, fields) => {
-      if (err) {
-        console.log(err);
-      } else {
-        res.render('index',{teams:teams})
-        }
-      })
+      });
+    }
   });
+});
+
+// 2번쨰 시도 multipleStatement true로 하고 한군데다가 다 쿼리문 쳐박기
+// app.get("/", (req, res) => {
+
+//   client.query( "Select * from free order by f_date DESC limit 4;Select * from secret order by s_date DESC limit 4;Select * from grad order by g_date DESC limit 4;Select * from market order by m_date DESC limit 4;Select * from new order by n_date DESC limit 4;Select * from info order by i_date DESC limit 4;Select * from prom order by p_date DESC limit 4;Select * from team order by t_date DESC limit 4", (err, frees, fields) => {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       res.render('index',{frees:frees})
+//       }
+//     })
+//   });
+
+// 1번째 시도 그냥 원시적으로 해본것
+// app.get("/", (req, res) => {
+//   const sql1 = "Select * from free order by f_date DESC limit 4";
+//   const sql2 = "Select * from secret order by s_date DESC limit 4";
+//   const sql3 = "Select * from grad order by g_date DESC limit 4";
+//   const sql4 = "Select * from market order by m_date DESC limit 4";
+//   const sql5 = "Select * from new order by n_date DESC limit 4";
+//   const sql6 = "Select * from info order by i_date DESC limit 4";
+//   const sql7 = "Select * from prom order by p_date DESC limit 4";
+//   const sql8 = "Select * from team order by t_date DESC limit 4";
+//   client.query(sql1, (err, frees, fields) => {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       res.render('index',{frees:frees})
+//       }
+//     })
+//     client.query(sql2, (err, secrets, fields) => {
+//       if (err) {
+//         console.log(err);
+//       } else {
+//         res.render('index',{secrets:secrets})
+//         }
+//       })
+//     client.query(sql3, (err, grads, fields) => {
+//       if (err) {
+//         console.log(err);
+//       } else {
+//         res.render('index',{grads:grads})
+//         }
+//       })
+//     client.query(sql4, (err, markets, fields) => {
+//       if (err) {
+//         console.log(err);
+//       } else {
+//         res.render('index',{markets:markets})
+//         }
+//       })
+//     client.query(sql5, (err, news, fields) => {
+//       if (err) {
+//         console.log(err);
+//       } else {
+//         res.render('index',{news:news})
+//         }
+//       })
+//     client.query(sql6, (err, infos, fields) => {
+//       if (err) {
+//         console.log(err);
+//       } else {
+//         res.render('index',{infos:infos})
+//         }
+//       })
+//     client.query(sql7, (err, proms, fields) => {
+//       if (err) {
+//         console.log(err);
+//       } else {
+//         res.render('index',{proms:proms})
+//         }
+//       })
+//     client.query(sql8, (err, teams, fields) => {
+//       if (err) {
+//         console.log(err);
+//       } else {
+//         res.render('index',{teams:teams})
+//         }
+// })
+// });
 
 app.get("/login", (req, res) => {
   res.render("login", { title: "login" });
