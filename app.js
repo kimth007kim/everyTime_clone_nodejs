487244f21fb310bbff7538daac2062f33a9b6cf4
kimth007kim <kimth007kim@naver.com>
2020-11-04 18:28:03 +0900

    로그인시 아이디 비번 체크 기능 등록

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const session = require("express-session");
+const FileStore = require("session-file-store")(session);
 var path = require("path");
 var mysql = require("mysql");
 var bodyParser = require("body-parser");
@@ -20,12 +22,33 @@ app.use(express.static("images"));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 app.use(cookieParser());
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    store: new FileStore(),
+  })
+);
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // 3번째 시도  두번째 쿼리를 콜백 함수 안에다가 집어넣기
 
+let user = {
+  uer_id: "kkd",
+  user_pwd: "1234",
+};
+
 app.get("/", (req, res) => {
+  console.log(req.session);
+
+  if (!req.session.num) {
+    req.session.num = 1;
+  } else {
+    req.session.num = req.session.num + 1;
+  }
+
   const sql1 = "Select * from free order by f_date DESC limit 4";
   const sql2 = "Select * from secret order by s_date DESC limit 4";
   const sql3 = "Select * from grad order by g_date DESC limit 4";
@@ -92,6 +115,7 @@ app.get("/", (req, res) => {
       });
     }
   });
+  // res.send(`Number:${req.session.num}`);
 });
 
 app.get("/free", (req, res) => {
@@ -99,7 +123,7 @@ app.get("/free", (req, res) => {
 });
 
 app.get("/login", (req, res) => {
-  res.render("login");
+  res.render("login", { pass: "tr" });
 });
 app.post("/login", (req, res) => {
   var id = req.body.userid;
@@ -115,21 +139,20 @@ app.post("/login", (req, res) => {
     } else {
       if (results.length > 0) {
         if (results[0].password == pw) {
-          res.send({
-            code: 200,
-            success: "login sucessfull",
-          });
+          // req.session.logined = ture;
+          // console.log(req.session.logined);
+          console.log("로그인 성공");
+
+          res.redirect("/");
         } else {
-          res.send({
-            code: 204,
-            sucess: "아이디와 비번 불일치",
-          });
+          console.log("false1");
+          res.render("login", { pass: "false1" });
+          // res.redirect("/");
         }
       } else {
-        res.send({
-          code: 204,
-          success: "Email does not exists",
-        });
+        // alert("아이디가 존재 하지 않습니다.")
+        console.log("false2");
+        res.render("login", { pass: "false2" });
       }
     }
   });
